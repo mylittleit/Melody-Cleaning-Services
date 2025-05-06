d62ddb5f1ff91487a72fb0897144fc69228ffe24
mylittleit <mylittleitltd@gmail.com>
2025-05-06 12:41:59 +0100

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -46,9 +46,9 @@ const Navbar = () => {
           <Image
             src="Melody.png"
             alt="Melody Cleaning Services Logo"
-            width={120}
+            width={140}
             height={40}
-            className="h-10 w-auto"
+            className="h-20 w-auto"
           />
         </Link>
 
