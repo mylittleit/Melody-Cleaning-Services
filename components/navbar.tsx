4a41c9ba1822bd480fa12e6a768b98bc33a745e7
mylittleit <mylittleitltd@gmail.com>
2025-05-06 12:19:37 +0100

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -44,10 +44,10 @@ const Navbar = () => {
       <div className="container flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center">
           <Image
-            src="/images/logo.png"
+            src="Logo.png"
             alt="Melody Cleaning Services Logo"
-            width={150}
-            height={50}
+            width={120}
+            height={30}
             className="h-auto w-auto"
           />
         </Link>
