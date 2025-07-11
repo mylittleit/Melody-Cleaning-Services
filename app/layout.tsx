cd6232149b798263c80fa75b52db128bdc7cedc3
mylittleit <mylittleitltd@gmail.com>
2025-07-11 08:52:00 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,7 @@ export default function RootLayout({
           <Navbar />
           <main>{children}</main>
           <Footer />
+          <WhatsAppButton />
         </ThemeProvider>
       </body>
     </html>
